fbe21e10828a099225250062ada0aad342b06231
VaheM27 <vmnatsakanyan27@gmail.com>
2022-10-05 21:54:07 +0400

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
-(function() {
+(function () {
   "use strict";
 
-  window.addEventListener('load', () => {
-    on_page_load()
+  window.addEventListener("load", () => {
+    on_page_load();
   });
 
   /**
@@ -11,30 +11,30 @@
   function on_page_load() {
     // Initialize On-scroll Animations
     AOS.init({
-      anchorPlacement: 'top-left',
+      anchorPlacement: "top-left",
       duration: 600,
       easing: "ease-in-out",
       once: true,
       mirror: false,
-      disable: 'mobile'
+      disable: "mobile",
     });
   }
 
   /**
    * Navbar effects and scrolltop buttons upon scrolling
    */
-  const navbar = document.getElementById('header-nav')
-  var body = document.getElementsByTagName("body")[0]
-  const scrollTop = document.getElementById('scrolltop')
+  const navbar = document.getElementById("header-nav");
+  var body = document.getElementsByTagName("body")[0];
+  const scrollTop = document.getElementById("scrolltop");
   window.onscroll = () => {
     if (window.scrollY > 0) {
-      navbar.classList.add('fixed-top', 'shadow-sm')
-      body.style.paddingTop = navbar.offsetHeight + "px"
+      navbar.classList.add("fixed-top", "shadow-sm");
+      body.style.paddingTop = navbar.offsetHeight + "px";
       scrollTop.style.visibility = "visible";
       scrollTop.style.opacity = 1;
     } else {
-      navbar.classList.remove('fixed-top', 'shadow-sm')
-      body.style.paddingTop = "0px"
+      navbar.classList.remove("fixed-top", "shadow-sm");
+      body.style.paddingTop = "0px";
       scrollTop.style.visibility = "hidden";
       scrollTop.style.opacity = 0;
     }
@@ -43,52 +43,53 @@
   /**
    * Masonry Grid
    */
-  var elem = document.querySelector('.grid');
-  if(elem) {
-    imagesLoaded(elem, function() {
+  var elem = document.querySelector(".grid");
+  if (elem) {
+    imagesLoaded(elem, function () {
       new Masonry(elem, {
-        itemSelector: '.grid-item',
+        itemSelector: ".grid-item",
         percentPosition: true,
-        horizontalOrder: true
+        horizontalOrder: true,
       });
-    })
+    });
   }
 
   /**
    * Big Picture Popup for images and videos
    */
-   document.querySelectorAll("[data-bigpicture]").forEach((function(e) {
-     e.addEventListener("click", (function(t){
-       t.preventDefault();
-       const data =JSON.parse(e.dataset.bigpicture)
-       BigPicture({
+  document.querySelectorAll("[data-bigpicture]").forEach(function (e) {
+    e.addEventListener("click", function (t) {
+      t.preventDefault();
+      const data = JSON.parse(e.dataset.bigpicture);
+      BigPicture({
         el: t.target,
-        ...data
-      })
-     })
-    )
-  }))
+        ...data,
+      });
+    });
+  });
 
   /**
    * Big Picture Popup for Photo Gallary
    */
-   document.querySelectorAll(".bp-gallery a").forEach((function(e) {
-    var caption = e.querySelector('figcaption')
-    var img = e.querySelector('img')
+  document.querySelectorAll(".bp-gallery a").forEach(function (e) {
+    var caption = e.querySelector("figcaption");
+    var img = e.querySelector("img");
     // set the link present on the item to the caption in full view
-    img.dataset.caption = '<a class="link-light" target="_blank" href="' + e.href + '">' + caption.innerHTML + '</a>';
-    window.console.log(caption, img)
-     e.addEventListener("click", (function(t){
-       t.preventDefault();
-       BigPicture({
+    img.dataset.caption =
+      '<a class="link-light" target="_blank" href="' +
+      e.href +
+      '">' +
+      caption.innerHTML +
+      "</a>";
+    window.console.log(caption, img);
+    e.addEventListener("click", function (t) {
+      t.preventDefault();
+      BigPicture({
         el: t.target,
-        gallery: '.bp-gallery',
-      })
-     })
-    )
-  }))
+        gallery: ".bp-gallery",
+      });
+    });
+  });
 
   // Add your javascript here
-
-
 })();
